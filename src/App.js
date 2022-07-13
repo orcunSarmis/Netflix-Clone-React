29838c83c8d3c67ce607e4d0b30913df88626067
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-07-13 23:16:36 +1000

    error of complie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage  from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
 
 function App() {
-  return <div></div>;
+  // localhost:3000/
+  return (
+
+  <div>
+    <Switch>
+      <Route path='/'>
+        <AllMeetupsPage />
+      </Route>
+      <Route path='/new-meetup'>
+        <NewMeetupPage />
+      </Route>
+      <Route path='/favorites'>
+        <FavoritesPage />
+      </Route>
+    </Switch>
+  </div>  
+  );
 }
 
 export default App;
